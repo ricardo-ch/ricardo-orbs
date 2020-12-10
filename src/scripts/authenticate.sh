13c1f03ab483057f7655dcd6a589c82dced82ff8
rnemet <robert.nemet@ricardo.ch>
2020-12-10 10:40:01 +0100

    [semver:major] the first version

---
+++ src/scripts/authenticate.sh
@@ -0,0 +1,7 @@
+# Authenticate to GCP. All needed values have to be already present in CircleCi context or enviroment.
+echo "${GCLOUD_SERVICE_KEY}" > "${HOME}"/gcp-key.json
+gcloud auth activate-service-account --key-file "${HOME}"/gcp-key.json
+gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
+gcloud --quiet config set compute/zone "${GKE_COMPUTE_ZONE}"
+gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
+kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "${GKE_CLUSTER_NAME}"
