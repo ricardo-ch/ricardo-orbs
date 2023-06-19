13f7af65a4923e28390fad773032c26310463332
golonzovsky <alex.golonzovsky@gmail.com>
2023-06-19 16:27:34 +0200

    [semver:patch] move GOOGLE_APPLICATION_CREDENTIALS set to step

--- src/scripts/authenticate.sh
+++ src/scripts/authenticate.sh
@@ -1,6 +1,5 @@
 # Authenticate to GCP. All needed values have to be already present in CircleCi context or environment.
 echo "${GCLOUD_SERVICE_KEY}" > "${HOME}"/gcp-key.json
-export GOOGLE_APPLICATION_CREDENTIALS="${HOME}"/gcp-key.json
 gcloud auth activate-service-account --key-file "${HOME}"/gcp-key.json
 gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GKE_COMPUTE_ZONE}"
