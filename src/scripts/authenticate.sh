4b0afa48e51c2597bd93e197632c5ad5c157727a
Robert Nemet <robert.nemet@gmail.com>
2024-02-21 09:21:25 +0100

    authenticate to dev-banana

--- src/scripts/authenticate.sh
+++ src/scripts/authenticate.sh
@@ -5,3 +5,7 @@ gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GKE_COMPUTE_ZONE}"
 gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
 kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "${GKE_CLUSTER_NAME}"
+# rename context from dev-banana to dev-cookie because isopod do not recognize dev-banana
+if [ "${GKE_CLUSTER_NAME}" = "dev-banana" ]; then
+  kubectl config rename-context "${GKE_CLUSTER_NAME}" "dev-cookie"
+fi
