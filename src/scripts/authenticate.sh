a9f5006fb4a7563202907fb38b09e22eefc3bf0a
Robert Nemet <robert.nemet@ricardo.ch>
2024-05-10 08:19:45 +0200

    [semver:patch] adding two context names (#113)
    * double context

--- src/scripts/authenticate.sh
+++ src/scripts/authenticate.sh
@@ -5,7 +5,9 @@ gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GKE_COMPUTE_ZONE}"
 gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
 kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "${GKE_CLUSTER_NAME}"
-# rename context from dev-banana to dev-cookie because isopod do not recognize dev-banana
+# If cluster is a "dev-banana" cluster, pull it again and rename it to "dev-cookie". Required for isopod as isopod uses "dev-cookie" context.
+# other steps like port-forward will fail if the context is "dev-cookie".
 if [ "${GKE_CLUSTER_NAME}" = "dev-banana" ]; then
-  kubectl config rename-context "${GKE_CLUSTER_NAME}" "dev-cookie"
+  gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
+  kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "dev-cookie"
 fi
