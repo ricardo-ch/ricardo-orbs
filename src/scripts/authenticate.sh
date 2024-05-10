edad6af558e23e8217435ebc34bf22aab90537ee
Robert Nemet <robert.nemet@gmail.com>
2024-05-10 07:58:10 +0200

    double context - comment

--- src/scripts/authenticate.sh
+++ src/scripts/authenticate.sh
@@ -5,7 +5,8 @@ gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GKE_COMPUTE_ZONE}"
 gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
 kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "${GKE_CLUSTER_NAME}"
-# If cluster is a "dev-banana" cluster, pull it again and rename it to "dev-cookie". Required for isopod to work
+# If cluster is a "dev-banana" cluster, pull it again and rename it to "dev-cookie". Required for isopod as isopod uses "dev-cookie" context.
+# other steps like port-forward will fail if the context is "dev-cookie".
 if [ "${GKE_CLUSTER_NAME}" = "dev-banana" ]; then
   gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
   kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "dev-cookie"
