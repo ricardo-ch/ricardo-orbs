4f25fdd5c185ae4dc097785e335a06238427b8b5
Robert Nemet <robert.nemet@ricardo.ch>
2024-05-27 13:44:31 +0200

    [semver:minor] add prod-tomato to authenticate, remove dev-cookie (#114)

--- src/scripts/authenticate.sh
+++ src/scripts/authenticate.sh
@@ -5,9 +5,9 @@ gcloud --quiet config set project "${GOOGLE_PROJECT_ID}"
 gcloud --quiet config set compute/zone "${GKE_COMPUTE_ZONE}"
 gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
 kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "${GKE_CLUSTER_NAME}"
-# If cluster is a "dev-banana" cluster, pull it again and rename it to "dev-cookie". Required for isopod as isopod uses "dev-cookie" context.
-# other steps like port-forward will fail if the context is "dev-cookie".
-if [ "${GKE_CLUSTER_NAME}" = "dev-banana" ]; then
+# If cluster is a "prod-tomato", pull it again and rename it to "prod-cherry". Required for isopod as it uses "prod-cherry" context.
+# other steps like port-forward will use "prod-tomato".
+if [ "${GKE_CLUSTER_NAME}" = "prod-tomato" ]; then
   gcloud --quiet container clusters get-credentials "${GKE_CLUSTER_NAME}" --zone "${GKE_COMPUTE_ZONE}"
-  kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "dev-cookie"
+  kubectl config rename-context gke_"${GOOGLE_PROJECT_ID}"_"${GKE_COMPUTE_ZONE}"_"${GKE_CLUSTER_NAME}" "prod-cherry"
 fi
